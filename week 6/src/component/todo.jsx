869c647116f9618a322415cc19b594b06b0cf04d
kav6778 <kavin.iitkgp@gmail.com>
2024-02-02 18:26:33 +0530

    Week 6 React Hooks

---
+++ week 6/src/component/todo.jsx
@@ -0,0 +1,12 @@
+function Todo({title, description}){
+    return <>
+        <h1>
+            {title}
+        </h1>
+        <h5>
+            {description}
+        </h5>
+    </>
+}
+
+export default Todo
